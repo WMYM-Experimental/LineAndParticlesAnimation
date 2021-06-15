ffdd1638d162a5635adc80701622cede9d927a75
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-06-14 22:21:48 -0500

    Add files via upload

---
+++ events.js
@@ -0,0 +1,40 @@
+//resize event whrn change the size screen
+window.addEventListener("resize", function () {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  mouse.radius =
+    (canvas.height / radiusDivider) * (canvas.width / radiusDivider);
+  init();
+});
+
+//mouse out event
+window.addEventListener("mouseout", function () {
+  mouse.x = null;
+  mouse.y = null;
+});
+
+//mouse repulsion function
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+//add more particles with clicks
+window.addEventListener("click", function (event) {
+  let size = getSize(minRangeSize, maxRangeSize);
+  let x = Math.random() * (innerWidth - size * 2 - size * 2) + size * 2;
+  let y = Math.random() * (innerHeight - size * 2 - size * 2) + size * 2;
+  let directionX = getDirection();
+  let directionY = getDirection();
+  let color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  particlesArray.push(
+    new Particle(mouse.x, mouse.y, directionX, directionY, size, color)
+  );
+});
+
+//delete particles with space bar
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    particlesArray.pop();
+  }
+});
