9e02cfeedb99b6e618a5c16fe44214ba8bfc85d9
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-06-14 19:02:31 -0500

    Create events.js

---
+++ events.js
@@ -0,0 +1,26 @@
+//repulsion with mouse 
+window.addEventListener('mousemove',
+    function (event) {
+        mouse.x = event.x;
+        mouse.y = event.y;
+    }
+);
+
+//mouse out event
+window.addEventListener("mouseout",
+    function () {
+        mouse.x = undefined;
+        mouse.y = undefined;
+        mouse.radius = undefined;
+    }
+);
+
+//resize event when change the screen size
+window.addEventListener("resize",
+    function () {
+        canvas.width = innerWidth;
+        canvas.height = innerHeight;
+        mouse.radius = (canvas.height / radiusDivider) * (canvas.width / radiusDivider);
+        init();
+    }
+);
