e4fa42ac2f7b90f7077cf79d138b1f480f42618b
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-06-14 19:30:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const radiusDivider = 100;
 const windowDivider = 10000;
 const opacityValueDivider = 25000;
 let particlesMultiplier = 1;
+
 //get mouse position
 let mouse = {
   x: null,
@@ -17,25 +18,42 @@ let mouse = {
   radius: (canvas.height / radiusDivider) * (canvas.width / radiusDivider),
 };
 
-document.addEventListener("keydown", (event) => {
-  if (event.code === "Space") {
-    particlesArray.pop();
-  }
-});
-
-//mouse out event
-window.addEventListener("mouseout", function () {
-  mouse.x = undefined;
-  mouse.y = undefined;
-});
-
-//resize event whrn change the size screen
 window.addEventListener("resize", function () {
   canvas.width = innerWidth;
   canvas.height = innerHeight;
   mouse.radius =
     (canvas.height / radiusDivider) * (canvas.width / radiusDivider);
   init();
+});
+
+//mouse out event
+window.addEventListener("mouseout", function () {
+  mouse.x = null;
+  mouse.y = null;
+});
+
+//add particles
+window.addEventListener("click", function (event) {
+  let size = getSize(minRangeSize, maxRangeSize);
+  let x = event.x;
+  let y = event.y;
+  let directionX = getDirection();
+  let directionY = getDirection();
+  let color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
+});
+
+//delete particles with space bar
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    particlesArray.pop();
+  }
+});
+
+//mouse repulsion function
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
 });
 
 class Particle {
