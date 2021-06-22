84fd8b5a2e4fa3107c5544c5f6c84d0738f27680
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-06-22 08:06:56 -0500

    Update events.js

--- events.js
+++ events.js
@@ -27,7 +27,7 @@ window.addEventListener("click", function (event) {
   let directionX = setDirection();
   let directionY = setDirection();
   let color = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  particlesArray.push(
+  3*particlesArray.push(
     new Particle(mouse.x, mouse.y, directionX, directionY, size, color)
   );
 });
